21355552016c9796e727348484ef6110332baa6c
DaniyalAhmad2 <daniyal.ahmad@athenasol.ai>
2024-12-05 02:19:58 +0500

    add track json

--- examples/soccer/main.py
+++ examples/soccer/main.py
@@ -9,8 +9,8 @@ import supervision as sv
 from tqdm import tqdm
 from ultralytics import YOLO
 import itertools
-
-from sports.annotators.soccer import draw_pitch, draw_points_on_pitch, draw_pitch_voronoi_diagram, draw_paths_on_pitch
+import json
+from sports.annotators.soccer import draw_pitch, draw_points_on_pitch, draw_pitch_voronoi_diagram
 from sports.common.ball import BallTracker, BallAnnotator
 from sports.common.team import TeamClassifier
 from sports.common.view import ViewTransformer
@@ -41,29 +41,10 @@ VERTEX_LABEL_ANNOTATOR = sv.VertexLabelAnnotator(
 VERTEX_ANNOTATOR = sv.VertexAnnotator(
     color=sv.Color.GREEN, radius=5
     )
-EDGE_ANNOTATOR = sv.EdgeAnnotator(
-    color=sv.Color.from_hex('#FF1493'),
-    thickness=2,
-    edges=CONFIG.edges,
-)
-TRIANGLE_ANNOTATOR = sv.TriangleAnnotator(
-    color=sv.Color.from_hex('#FF1493'),
-    base=20,
-    height=15,
-)
-BOX_ANNOTATOR = sv.BoxAnnotator(
-    color=sv.ColorPalette.from_hex(COLORS),
-    thickness=2
-)
+
 ELLIPSE_ANNOTATOR = sv.EllipseAnnotator(
     color=sv.ColorPalette.from_hex(COLORS),
     thickness=2
-)
-BOX_LABEL_ANNOTATOR = sv.LabelAnnotator(
-    color=sv.ColorPalette.from_hex(COLORS),
-    text_color=sv.Color.from_hex('#FFFFFF'),
-    text_padding=5,
-    text_thickness=1,
 )
 ELLIPSE_LABEL_ANNOTATOR = sv.LabelAnnotator(
     color=sv.ColorPalette.from_hex(COLORS),
@@ -181,170 +162,16 @@ def render_radar(
     
     return radar
 
-
-def run_pitch_detection(source_video_path: str, device: str) -> Iterator[np.ndarray]:
+def save_tracking_data_to_json(output_path, data):
     """
-    Run pitch detection on a video and yield annotated frames.
-
+    Save tracking data to a JSON file.
+    
     Args:
-        source_video_path (str): Path to the source video.
-        device (str): Device to run the model on (e.g., 'cpu', 'cuda').
-
-    Yields:
-        Iterator[np.ndarray]: Iterator over annotated frames.
+        output_path (str): Path to save the JSON file.
+        data (dict): Data to save.
     """
-    pitch_detection_model = YOLO(PITCH_DETECTION_MODEL_PATH).to(device=device)
-    frame_generator = sv.get_video_frames_generator(source_path=source_video_path)
-    for frame in frame_generator:
-        result = pitch_detection_model(frame, verbose=False)[0]
-        keypoints = sv.KeyPoints.from_ultralytics(result)
-
-        annotated_frame = frame.copy()
-        annotated_frame = VERTEX_LABEL_ANNOTATOR.annotate(
-            annotated_frame, keypoints, CONFIG.labels)
-        yield annotated_frame
-
-
-def run_player_detection(source_video_path: str, device: str) -> Iterator[np.ndarray]:
-    """
-    Run player detection on a video and yield annotated frames.
-
-    Args:
-        source_video_path (str): Path to the source video.
-        device (str): Device to run the model on (e.g., 'cpu', 'cuda').
-
-    Yields:
-        Iterator[np.ndarray]: Iterator over annotated frames.
-    """
-    player_detection_model = YOLO(PLAYER_DETECTION_MODEL_PATH).to(device=device)
-    frame_generator = sv.get_video_frames_generator(source_path=source_video_path)
-    for frame in frame_generator:
-        result = player_detection_model(frame, imgsz=1280, verbose=False)[0]
-        detections = sv.Detections.from_ultralytics(result)
-
-        annotated_frame = frame.copy()
-        annotated_frame = BOX_ANNOTATOR.annotate(annotated_frame, detections)
-        annotated_frame = BOX_LABEL_ANNOTATOR.annotate(annotated_frame, detections)
-        yield annotated_frame
-
-
-def run_ball_detection(source_video_path: str, device: str) -> Iterator[np.ndarray]:
-    """
-    Run ball detection on a video and yield annotated frames.
-
-    Args:
-        source_video_path (str): Path to the source video.
-        device (str): Device to run the model on (e.g., 'cpu', 'cuda').
-
-    Yields:
-        Iterator[np.ndarray]: Iterator over annotated frames.
-    """
-    ball_detection_model = YOLO(BALL_DETECTION_MODEL_PATH).to(device=device)
-    frame_generator = sv.get_video_frames_generator(source_path=source_video_path)
-    ball_tracker = BallTracker(buffer_size=20)
-    ball_annotator = BallAnnotator(radius=6, buffer_size=10)
-
-    def callback(image_slice: np.ndarray) -> sv.Detections:
-        result = ball_detection_model(image_slice, imgsz=640, verbose=False)[0]
-        return sv.Detections.from_ultralytics(result)
-
-    slicer = sv.InferenceSlicer(
-        callback=callback,
-        # overlap_filter_strategy=sv.OverlapFilter.NONE,
-        slice_wh=(640, 640),
-    )
-
-    for frame in frame_generator:
-        detections = slicer(frame).with_nms(threshold=0.1)
-        detections = ball_tracker.update(detections)
-        annotated_frame = frame.copy()
-        annotated_frame = ball_annotator.annotate(annotated_frame, detections)
-        yield annotated_frame
-
-
-def run_player_tracking(source_video_path: str, device: str) -> Iterator[np.ndarray]:
-    """
-    Run player' tracking on a video and yield annotated frames with tracked players.
-
-    Args:
-        source_video_path (str): Path to the source video.
-        device (str): Device to run the model on (e.g., 'cpu', 'cuda').
-
-    Yields:
-        Iterator[np.ndarray]: Iterator over annotated frames.
-    """
-    player_detection_model = YOLO(PLAYER_DETECTION_MODEL_PATH).to(device=device)
-    frame_generator = sv.get_video_frames_generator(source_path=source_video_path)
-    tracker = sv.ByteTrack(minimum_consecutive_frames=3)
-    for frame in frame_generator:
-        result = player_detection_model(frame, imgsz=1280, verbose=False)[0]
-        detections = sv.Detections.from_ultralytics(result)
-        detections = tracker.update_with_detections(detections)
-
-        labels = [str(tracker_id) for tracker_id in detections.tracker_id]
-
-        annotated_frame = frame.copy()
-        annotated_frame = ELLIPSE_ANNOTATOR.annotate(annotated_frame, detections)
-        annotated_frame = ELLIPSE_LABEL_ANNOTATOR.annotate(
-            annotated_frame, detections, labels=labels)
-        yield annotated_frame
-
-
-def run_team_classification(source_video_path: str, device: str) -> Iterator[np.ndarray]:
-    """
-    Run team classification on a video and yield annotated frames with team colors.
-
-    Args:
-        source_video_path (str): Path to the source video.
-        device (str): Device to run the model on (e.g., 'cpu', 'cuda').
-
-    Yields:
-        Iterator[np.ndarray]: Iterator over annotated frames.
-    """
-    player_detection_model = YOLO(PLAYER_DETECTION_MODEL_PATH).to(device=device)
-    frame_generator = sv.get_video_frames_generator(
-        source_path=source_video_path, stride=STRIDE)
-
-    crops = []
-    for frame in tqdm(frame_generator, desc='collecting crops'):
-        result = player_detection_model(frame, imgsz=1280, verbose=False)[0]
-        detections = sv.Detections.from_ultralytics(result)
-        crops += get_crops(frame, detections[detections.class_id == PLAYER_CLASS_ID])
-
-    team_classifier = TeamClassifier(device=device)
-    team_classifier.fit(crops)
-
-    frame_generator = sv.get_video_frames_generator(source_path=source_video_path)
-    tracker = sv.ByteTrack(minimum_consecutive_frames=3)
-    for frame in frame_generator:
-        result = player_detection_model(frame, imgsz=1280, verbose=False)[0]
-        detections = sv.Detections.from_ultralytics(result)
-        detections = tracker.update_with_detections(detections)
-
-        players = detections[detections.class_id == PLAYER_CLASS_ID]
-        crops = get_crops(frame, players)
-        players_team_id = team_classifier.predict(crops)
-
-        goalkeepers = detections[detections.class_id == GOALKEEPER_CLASS_ID]
-        goalkeepers_team_id = resolve_goalkeepers_team_id(
-            players, players_team_id, goalkeepers)
-
-        referees = detections[detections.class_id == REFEREE_CLASS_ID]
-
-        detections = sv.Detections.merge([players, goalkeepers, referees])
-        color_lookup = np.array(
-                players_team_id.tolist() +
-                goalkeepers_team_id.tolist() +
-                [REFEREE_CLASS_ID] * len(referees)
-        )
-        labels = [str(tracker_id) for tracker_id in detections.tracker_id]
-
-        annotated_frame = frame.copy()
-        annotated_frame = ELLIPSE_ANNOTATOR.annotate(
-            annotated_frame, detections, custom_color_lookup=color_lookup)
-        annotated_frame = ELLIPSE_LABEL_ANNOTATOR.annotate(
-            annotated_frame, detections, labels, custom_color_lookup=color_lookup)
-        yield annotated_frame
+    with open(output_path, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
 
 
 def run_radar(source_video_path: str, device: str) -> Iterator[np.ndarray]:
@@ -353,34 +180,14 @@ def run_radar(source_video_path: str, device: str) -> Iterator[np.ndarray]:
     ball_detection_model = YOLO(BALL_DETECTION_MODEL_PATH).to(device=device)
 
 
-    # frame_generator = sv.get_video_frames_generator(source_path=source_video_path)
-    # ball_tracker = BallTracker(buffer_size=20)
-    # ball_annotator = BallAnnotator(radius=6, buffer_size=10)
-
-    # def callback(image_slice: np.ndarray) -> sv.Detections:
-    #     result = ball_detection_model(image_slice, imgsz=640, verbose=False)[0]
-    #     return sv.Detections.from_ultralytics(result)
-
-    # slicer = sv.InferenceSlicer(
-    #     callback=callback,
-    #     # overlap_filter_strategy=sv.OverlapFilter.NONE,
-    #     slice_wh=(640, 640),
-    # )
-
-    # for frame in frame_generator:
-    # detections = slicer(frame).with_nms(threshold=0.1)
-    # detections = ball_tracker.update(detections)
-    # annotated_frame = frame.copy()
-    # annotated_frame = ball_annotator.annotate(annotated_frame, detections)
-        # yield annotated_frame
-
-
     frame_generator = sv.get_video_frames_generator(
         source_path=source_video_path, stride=STRIDE)
 
     crops = []
     tracker_id_to_team_id = {}
+    tracking_data = {"frames": []}
     frame_count = 0
+
     for frame in tqdm(frame_generator, desc='collecting crops'):
         frame_count += 1
         print(frame_count)
@@ -456,6 +263,38 @@ def run_radar(source_video_path: str, device: str) -> Iterator[np.ndarray]:
             goalkeepers_team_id.tolist() +
             [REFEREE_CLASS_ID] * len(referees)
         )
+        if detections_ball and detections_ball.tracker_id is not None:
+            ball_tracks = [
+                {
+                    "id": ball_id,
+                    "bbox": bbox.tolist()
+                }
+                for ball_id, bbox in zip(detections_ball.tracker_id, detections_ball.xyxy)
+            ]
+        else:
+            ball_tracks = []
+        
+        if detections and detections.tracker_id is not None:
+            object_tracks = [
+                {
+                    "id": int(tracker_id),
+                    "bbox": bbox.tolist(),
+                    "team_id": int(tracker_id_to_team_id.get(tracker_id, -1))
+                }
+                for tracker_id, bbox in zip(detections.tracker_id, detections.xyxy)
+            ]
+        else:
+            object_tracks = []
+
+
+        frame_data = {
+            "frame_id": frame_count,
+            "keypoints": keypoints.xy[0].tolist(),
+            "object_tracks": object_tracks,
+            
+            "ball_tracks": ball_tracks
+        }
+        tracking_data["frames"].append(frame_data)
         labels = [str(tracker_id) for tracker_id in detections.tracker_id]
 
         annotated_frame = frame.copy()
@@ -488,9 +327,10 @@ def run_radar(source_video_path: str, device: str) -> Iterator[np.ndarray]:
         annotated_frame = sv.draw_image(annotated_frame, radar_resized, opacity=0.5, rect=rect)
         
         yield [annotated_frame, radar]
+    save_tracking_data_to_json("output-data.json", tracking_data)
 
 
-def main(source_video_path: str, target_video_path: str, device: str, mode: Mode) -> None:
+def main(source_video_path: str, target_video_path: str, device: str) -> None:
     # Check if 'output' directory exists
     output_dir = os.path.join(os.getcwd(), 'output')
     if not os.path.exists(output_dir):
@@ -502,60 +342,36 @@ def main(source_video_path: str, target_video_path: str, device: str, mode: Mode
     target_video_path = os.path.join(output_dir, base_name)
     target_video_path_radar = os.path.join(output_dir, f"{name}_radar{ext}")
 
-    # Initialize frame generator based on mode
-    if mode == Mode.PITCH_DETECTION:
-        frame_generator = run_pitch_detection(source_video_path=source_video_path, device=device)
-    elif mode == Mode.PLAYER_DETECTION:
-        frame_generator = run_player_detection(source_video_path=source_video_path, device=device)
-    elif mode == Mode.BALL_DETECTION:
-        frame_generator = run_ball_detection(source_video_path=source_video_path, device=device)
-    elif mode == Mode.PLAYER_TRACKING:
-        frame_generator = run_player_tracking(source_video_path=source_video_path, device=device)
-    elif mode == Mode.TEAM_CLASSIFICATION:
-        frame_generator = run_team_classification(source_video_path=source_video_path, device=device)
-    elif mode == Mode.RADAR:
-        frame_generator = run_radar(source_video_path=source_video_path, device=device)
-    else:
-        raise NotImplementedError(f"Mode {mode} is not implemented.")
 
+    frame_generator = run_radar(source_video_path=source_video_path, device=device)
     video_info = sv.VideoInfo.from_video_path(source_video_path)
 
-    if mode == Mode.RADAR:
-        # Retrieve the first frame to determine radar frame size
-        first_frame = next(frame_generator)
-        annotated_frame = first_frame[0]
-        radar_frame = first_frame[1]
+    # Retrieve the first frame to determine radar frame size
+    first_frame = next(frame_generator)
+    annotated_frame = first_frame[0]
+    radar_frame = first_frame[1]
 
-        # Create video_info for radar frames
-        video_info_radar = sv.VideoInfo(
-            width=radar_frame.shape[1],
-            height=radar_frame.shape[0],
-            fps=video_info.fps
-        )
+    # Create video_info for radar frames
+    video_info_radar = sv.VideoInfo(
+        width=radar_frame.shape[1],
+        height=radar_frame.shape[0],
+        fps=video_info.fps
+    )
 
-        # Re-create the frame generator including the first frame
-        frame_generator = itertools.chain([first_frame], frame_generator)
+    # Re-create the frame generator including the first frame
+    frame_generator = itertools.chain([first_frame], frame_generator)
 
-        # Create two VideoSink objects for frame and radar videos
-        with sv.VideoSink(target_video_path, video_info) as frame_sink, \
-             sv.VideoSink(target_video_path_radar, video_info_radar) as radar_sink:
-            for frame in frame_generator:
-                frame_sink.write_frame(frame[0])
-                radar_sink.write_frame(frame[1])
+    # Create two VideoSink objects for frame and radar videos
+    with sv.VideoSink(target_video_path, video_info) as frame_sink, \
+            sv.VideoSink(target_video_path_radar, video_info_radar) as radar_sink:
+        for frame in frame_generator:
+            frame_sink.write_frame(frame[0])
+            radar_sink.write_frame(frame[1])
 
-                cv2.imshow("frame", frame[0])
-                cv2.imshow("radar", frame[1])
-                if cv2.waitKey(1) & 0xFF == ord("q"):
-                    break
-    else:
-        with sv.VideoSink(target_video_path, video_info) as sink:
-            for frame in frame_generator:
-                # print("in else")
-                sink.write_frame(frame)
-
-                cv2.imshow("frame", frame)
-                if cv2.waitKey(1) & 0xFF == ord("q"):
-                    break
+            cv2.imshow("frame", frame[0])
+            cv2.imshow("radar", frame[1])
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
 
     cv2.destroyAllWindows()
 
@@ -565,11 +381,9 @@ if __name__ == '__main__':
     parser.add_argument('--source_video_path', type=str, required=True)
     parser.add_argument('--target_video_path', type=str, required=True)
     parser.add_argument('--device', type=str, default='cpu')
-    parser.add_argument('--mode', type=Mode, default=Mode.PLAYER_DETECTION)
     args = parser.parse_args()
     main(
         source_video_path=args.source_video_path,
         target_video_path=args.target_video_path,
-        device=args.device,
-        mode=args.mode
+        device=args.device
     )
